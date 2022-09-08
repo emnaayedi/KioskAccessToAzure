39ecc4c58cd377b6f5f1a0bb38edb4cb1ac32551
emnaayedi <49609035+emnaayedi@users.noreply.github.com>
2022-09-08 12:49:21 -0500

    Add application code to access azure storage with authentication

---
+++ KioskAccessToAzure/RegistraionForm.aspx.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KioskAccessToAzure
+{
+    public partial class RegistraionForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        { 
+
+            if (TextBox3.Text == "Aquila2020#")
+            {
+                Server.Transfer("RedirectToPDF.aspx");
+
+            }
+
+        }
+    }
+}
